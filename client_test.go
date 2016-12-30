76c0dfa63443ae060ab7aa42a8c4aed131224f76
Aliaksandr Pliutau <pliutau.aliaksandr@lazada.com>
2016-12-30 11:38:59 +0700

    tests fix

--- client_test.go
+++ client_test.go
@@ -7,14 +7,15 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	c := New("http://127.0.0.1:8081")
+	c := New("https://as.weelco.com/liveapi")
 
 	// Get stream by hash
-	s, e := c.GetStream("6BF8AA3990AEAC71D3CD0476095F08F6")
+	s, e := c.GetStream("75AB8BF9947A9E7C23A2B026CF623581")
 	fmt.Printf("Stream: %v, Error: %v\n", s, e)
 
 	// Update stream's info
-	s.URL = fmt.Sprintf("http://127.0.0.1:1935/%d", time.Now().Unix())
+	s.OutputURL = fmt.Sprintf("http://127.0.0.1:1935/%d", time.Now().Unix())
+	s.OutputNodeAddr = fmt.Sprintf("http://127.0.0.1:1936/%d", time.Now().Unix())
 	e = c.UpdateStream(s)
 	fmt.Printf("Stream: %v, Error: %v\n", s, e)
 }
